c73016fbd9895823c5ee8dc7ee2b60d89561df41
Alessandro Bahgat <ale.bahgat@gmail.com>
2010-03-08 00:28:08 +0100

    Added more supported torrent sites

--- bitlet4chrome/torrentUrls.js
+++ bitlet4chrome/torrentUrls.js
@@ -25,10 +25,12 @@ var torrentLinkFormats =
     sumotorrent : /^http:\/\/torrents\.sumotorrent\.com\/download\/*/,
     torrentportal : /^http:\/\/torrentportal\.com\/download\/*/,
     meganova : /^http:\/\/www\.meganova\.org\/download\/*/,
-    torrentreactorto : /^http:\/\/www\.torrentreactor\.to\/torrents\/download_\d+/,
+    torrentreactorto : /^http:\/\/www\.torrentreactor\.to\/torrents\/download\/\d+/,
     torrentreactornet : /^http:\/\/dl\.torrentreactor\.net\/download\.php\?id=.+/,
     torrentbox : /^http:\/\/torrentbox\.com\/download\/.+/,
     torrentmatrix : /^http:\/\/www\.torrentmatrix\.com\/download\.php\?id=.+/,
     bitenova : /^http:\/\/www\.bitenova\.nl\/download\.php\?id=.+/,
-    demonoid : /^http:\/\/www\.demonoid\.com\/files\/download\/.+/
+    demonoid : /^http:\/\/www\.demonoid\.com\/files\/download\/.+/,
+	torrentzap: /^http:\/\/torrentzap.com\/download\/\d+\/\d+/,
+	bitenova: /^http:\/\/www\.bitenova\.org\/download\.php\?id=.+/
 }
